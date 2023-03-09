54901b580af61c0a2c4e2f6bf9a069ad64419aa4
Oleksii Movchaniuk <oleksii.movchaniuk@fractal.ai>
2023-03-09 16:58:58 +0000

    url updated

--- okta-sample-frontend/src/environments/environment.prod.ts
+++ okta-sample-frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   oktaDomain: "dev-43780148.okta.com",
   oktaClientId: "0oa7x1m0wlNnvT1G95d7",
-  apiUrl: "https://iap-fractal-dev.azurewebsites.net/api/",
+  apiUrl: "https://okta-sample-dev.azurewebsites.net/api/",
 };
