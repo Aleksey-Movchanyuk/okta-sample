9617b0223d5fc6dc04a1a8ae791eda3033537257
Aleksey Movchanyuk <aleksey.movchanyuk@dipocket.org>
2023-03-12 10:58:22 +0000

    okta configuration update

--- okta-sample-frontend/src/environments/environment.prod.ts
+++ okta-sample-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   oktaDomain: "dev-43780148.okta.com",
-  oktaClientId: "0oa7x1m0wlNnvT1G95d7",
-  apiUrl: "https://okta-sample-dev.azurewebsites.net/api/",
+  oktaClientId: "0oa8kbfrabKRT7JAv5d7",
+  apiUrl: "https://okta-sample.azurewebsites.net/api/",
 };
